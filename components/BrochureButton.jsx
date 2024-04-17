82d82952d70b45c8d84ac666e858c50682d2671e
coreyr <coreystephenr@gmail.com>
2024-04-17 13:05:10 +1000

    brochure update commit

--- components/BrochureButton.jsx
+++ components/BrochureButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BrochureButton = () => {
     return (
         <div className='brochure_button'>
-            <a href='/images/HPBrochure.pdf' download>
+            <a href='/images/HPAPR2024.pdf' download>
                 <button type='button'>DOWNLOAD BROCHURE & PLANS</button>
             </a>
         </div>
